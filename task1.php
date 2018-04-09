4933e1deb67d5614912c426a4b119482ab062a2d
Luthfulahi <luteeph@gmail.com>
2018-04-09 11:43:54 +0100

    first commit

---
+++ task1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First Task</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<div id="container">
+		<div id="content">
+			<p id="welcome">
+				Welcome To Hotels NG <br> Internship 4.0
+			</p>
+
+			<p id="time">
+				CURRENT TIME : <?php echo date("h:i:sa") ?>
+			</p>
+		</div>
+	</div>
+
+</body>
+</html>
